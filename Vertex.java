f59d116ca57db5702811e336a2fa19a7bc030e09
M7chi <michicool080808@gmail.com>
2024-09-12 15:30:52 +0200

    skalarMult

--- Vertex.java
+++ Vertex.java
@@ -13,19 +13,30 @@ public Vertex(double x, double y) {
 public static void main(String[] args) {
     Vertex v1 = new Vertex(42, 17);
     Vertex v2 = new Vertex(0.3, 24.6);
+    Vertex v3 = v1.skalarMult(2);
     System.out.println(v1);
     System.out.println(v1.length());
     System.out.println(v2.length());
+    System.out.println(v3);
 
 }
 
 public double length(){
-
     return Math.sqrt(x*x+y*y);
+}
+
+public Vertex skalarMult(double s){
+    return new Vertex(x*s, y*s);
 
 }
 
-//test 
+public void skalarMultMod(double s){
+    x=x*s;
+    y=y*s;
+
+}
+
+ 
 public String toString(){
     return "(" + x + ", " + y + ")";
     }
