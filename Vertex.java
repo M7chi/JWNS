fd95c577c37e7577837de4bdf92049773ce2015b
Michi <100426482+M7chi@users.noreply.github.com>
2024-09-12 15:19:24 +0200

    Create Vertex.java

---
+++ Vertex.java
@@ -0,0 +1,31 @@
+
+
+class Vertex{
+    public double x;
+    public double y;
+
+public Vertex(double x, double y) {
+    this.x=x;
+    this.y=y;
+    }
+
+public static void main(String[] args) {
+    Vertex v1 = new Vertex(42, 17);
+    Vertex v2 = new Vertex(0.3, 24.6);
+    System.out.println(v1);
+    System.out.println(v1.length());
+    System.out.println(v2.length());
+
+}
+
+public double length(){
+
+    return Math.sqrt(x*x+y*y);
+
+}
+
+//test 
+public String toString(){
+    return "(" + x + ", " + y + ")";
+    }
+}
